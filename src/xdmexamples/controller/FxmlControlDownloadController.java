af197b7cef83431732a0bf576d9510fddfa209b8
Sakit <sakit.a@hotmail.com>
2015-11-16 16:10:57 +0400

    new version download tab works done 60%

--- src/xdmexamples/controller/FxmlControlDownloadController.java
+++ src/xdmexamples/controller/FxmlControlDownloadController.java
@@ -1,389 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package xdmexamples.controller;
 
-import com.sun.javafx.binding.StringFormatter;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
-import java.net.Socket;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.application.Platform;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TitledPane;
-import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import xdmexamples.preloader.Keys;
 
-/**
- * FXML Controller class
- *
- * @author sakit
- */
-public class FxmlControlDownloadController implements Initializable {
+public class FxmlControlDownloadController implements Initializable{
 
-    /*-------------------------------------FXML decleration---------------------------*/
-    @FXML
-    private Text fileAddress;
-    @FXML
-    private Text percentage;
-    @FXML
-    private Text dwnFileSize;
-    @FXML
-    private Text txtSize;
-    @FXML
-    private Button btnStart;
-    @FXML
-    private Button btnStop;
-    @FXML
-    private Button btnPause;
-    @FXML
-    private Button btnResume;
-    @FXML
-    private Button btnOpen;
-    @FXML
-    private ProgressBar prgBar;
-    @FXML
-    private ResourceBundle bundle;
-    @FXML
-    private TitledPane titledPane;
-    /*----------------------------------End of FXML decleration-----------------------*/
-
-    private URL downloadURL;
-    private InputStream inputStream;
-    private OutputStream outputStream;
-    private byte[] buffer;
-
-    private String fileName;
-    private String fileLocation;
-    private int fileSize;
-    private boolean dwnStop;
-    private boolean dwnSuspend;
-    private int bytesRead;
-    private final static int BUFFER_SIZE = 1024;
-    private final BooleanProperty ready = new SimpleBooleanProperty(false);
-    private Task worker;
-    private Thread thread;
-
-    /**
-     * Initializes the controller class.
-     *
-     * @param url
-     * @param rb
-     */
+    /*================================FXML Decleration============================*/
+    @FXML private TitledPane titledPane;
+    @FXML private Text fileAddress;
+    @FXML private ProgressBar prgBar;
+    @FXML private Label percentage;
+    @FXML private Button btnStart;
+    @FXML private Button btnCancel;
+    @FXML private Button btnResume;
+    @FXML private Button btnPause;
+    @FXML private Button btnOpen;
+    /*============================End of FXML Decleration=========================*/
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        bundle = rb;
-        dwnStop = false;
-        dwnSuspend = false;
-        btnStart.setDisable(false);
-        btnStop.setDisable(true);
-        btnPause.setDisable(true);
-        btnResume.setDisable(true);
-        btnOpen.setDisable(true);
-    }
-
-    @FXML
-    private void dwnStart() {
-        boolean connection = checkInternetConnection();
-        if (connection) {
-            btnStart.setDisable(true);
-            btnStop.setDisable(false);
-            btnPause.setDisable(false);
-            btnResume.setDisable(true);
-            btnOpen.setDisable(true);
-
-            worker = createTask();
-            worker.valueProperty().addListener(new ChangeListener<Object>() {
-
-                @Override
-                public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
-                    if (!(newValue instanceof Boolean)) {
-                        int per = (int) (100.0 * (Integer) newValue / fileSize);
-                        percentage.setText(String.valueOf(per) + " %");
-                        dwnFileSize.setText(newValue.toString() + " bytes");
-                    }
-                }
-            });
-
-            prgBar.setProgress(0);
-            prgBar.progressProperty().unbind();
-            prgBar.progressProperty().bind(worker.progressProperty());
-
-            ready.addListener((ObservableValue<? extends Boolean> ov, Boolean t, Boolean t1) -> {
-                if (Boolean.TRUE.equals(t1)) {
-                    Platform.runLater(() -> {
-                        System.out.println("Download finished");
-                        cleanSource();
-                        //ready.setValue(Boolean.FALSE);
-                    });
-                }
-            });
-            thread = new Thread(worker);
-            thread.setName(fileName);
-            //thread.setDaemon(true);
-            thread.start();
-            System.out.println(thread.getName()+" thread started");
-        }
-    }
-
-    @FXML
-    private void dwnStop() {
-        setStopped(true);
-        btnStart.setDisable(false);
-        btnStop.setDisable(true);
-        btnPause.setDisable(true);
-        btnResume.setDisable(true);
-        btnOpen.setDisable(true);
-    }
-
-    @FXML
-    private void dwnPause() {
-        setSuspended(true);
-        btnPause.setDisable(true);
-        btnResume.setDisable(false);
-    }
-
-    @FXML
-    private void dwnResume() {
-        btnPause.setDisable(false);
-        btnResume.setDisable(true);
-        resumeThread();
-    }
-
-    @FXML
-    private void openFile() {
-    }
-
-    public void initData(String url, String fileName, String fileLocation) {
-
-        this.fileName = fileName;
-        this.fileLocation = fileLocation;
-        bytesRead = 0;
-        String path = "";
-        try {
-            downloadURL = new URL(url);
-            URLConnection urlConnection = downloadURL.openConnection();
-            fileSize = urlConnection.getContentLength();
-            if (fileSize == -1) {
-                throw new FileNotFoundException(url);
-            }
-
-            buffer = new byte[BUFFER_SIZE];
-            String ext = url.substring(url.lastIndexOf("."));
-            path = fileLocation + File.separator + fileName + ext;
-            File file = new File(path);
-            outputStream = new FileOutputStream(file);
-            inputStream = new BufferedInputStream(
-                    urlConnection.getInputStream());
-
-        } catch (MalformedURLException | FileNotFoundException ex) {
-            showErrorMessage(ex.getMessage(), ex);
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
-            showErrorMessage(ex.getMessage(), ex);
-        }
-
-        titledPane.setText(fileName + "(" + path + ")");
-        fileAddress.setText(url);
-        txtSize.setText(String.valueOf(fileSize) + " bytes");
-    }
-
-    private void showErrorMessage(String message, Exception... excp) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(bundle.getString(Keys.ERROR));
-        alert.setHeaderText(bundle.getString(Keys.PRG_NAME));
-        alert.setContentText(message);
-
-        if (excp.length > 0) {
-            // Create expandable Exception.
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            excp[0].printStackTrace(pw);
-            String exceptionText = sw.toString();
-
-            Label label = new Label(bundle.getString(Keys.EXCEPTİON_LABEL));
-
-            TextArea textArea = new TextArea(exceptionText);
-            textArea.setEditable(false);
-            textArea.setWrapText(true);
-
-            textArea.setMaxWidth(Double.MAX_VALUE);
-            textArea.setMaxHeight(Double.MAX_VALUE);
-            GridPane.setVgrow(textArea, Priority.ALWAYS);
-            GridPane.setHgrow(textArea, Priority.ALWAYS);
-
-            GridPane expContent = new GridPane();
-            expContent.setMaxWidth(Double.MAX_VALUE);
-            expContent.add(label, 0, 0);
-            expContent.add(textArea, 0, 1);
-
-            // Set expandable Exception into the dialog pane.
-            alert.getDialogPane().setExpandableContent(expContent);
-        }
-        Platform.runLater(() -> {
-            alert.showAndWait();
-        });
-
-    }
-
-    private Task createTask() {
-        return new Task() {
-            @Override
-            protected Object call() throws Exception {
-                int byteCount = -1;
-                //A correct implementation of a Task will always check for cancellation.
-                while (!isStopped() && (bytesRead < fileSize) && !dwnStop) {
-                    try {
-                        byteCount = inputStream.read(buffer);
-                        if (byteCount == -1) {
-                            dwnStop = true;
-                            break;
-                        } else {
-                            outputStream.write(buffer, 0, byteCount);
-                            bytesRead += byteCount;
-                            updateProgress(bytesRead, fileSize);
-                            //updateMessage(String.valueOf(bytesRead));
-                            updateValue(bytesRead);
-                        }
-                    } catch (IOException ioe) {
-                        dwnStop = true;
-                        showErrorMessage(ioe.getMessage(), ioe);
-                        break;
-                    }finally{
-                        setStopped(true);
-                    }
-                    synchronized (this) {
-                        try {
-                            if (isSuspended()) {
-                                System.out.println(thread.getName() + " thread waiting....");
-                                setSuspended(false);
-                                this.wait();
-                            }
-                        } catch (InterruptedException e) {
-                            showErrorMessage(e.getMessage(), e);
-                        }
-                    }
-                }
-                outputStream.close();
-                inputStream.close();
-                ready.setValue(byteCount >= fileSize);
-                return true;
-            }
-        };
-    }
-
-    private boolean checkInternetConnection() {
-        Socket socket = new Socket();
-        InetSocketAddress adrs = new InetSocketAddress("www.google.com", 80);
-        try {
-            socket.connect(adrs, 3000);
-            return true;
-        } catch (Exception e) {
-            showDialog(Alert.AlertType.ERROR, bundle.getString(Keys.ERROR), bundle.getString(Keys.PRG_NAME),
-                    bundle.getString(Keys.CHK_INT));
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException ex) {
-                Logger.getLogger(FxmlChartController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        return false;
-    }
-
-    private void showDialog(Alert.AlertType type, String title, String head, String message) {
-        Alert alert = new Alert(type);
-        alert.setTitle(title);
-        alert.setHeaderText(head);
-        alert.setContentText(message);
-        Stage st = (Stage) alert.getDialogPane().getScene().getWindow();
-        st.getIcons().add(new Image(getClass().getResourceAsStream("/resources/images/azer_emblem.png")));
-
-        if (type == Alert.AlertType.CONFIRMATION) {
-            ButtonType btnYes = new ButtonType(bundle.getString(Keys.YES), ButtonBar.ButtonData.YES);
-            ButtonType btnCancel = new ButtonType(bundle.getString(Keys.CANCEL), ButtonBar.ButtonData.CANCEL_CLOSE);
-
-            alert.getButtonTypes().setAll(btnYes, btnCancel);
-
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.get() == btnYes) {
-
-            }
-        } else {
-            alert.showAndWait();
-        }
-    }
-
-    private void cleanSource() {
-        System.out.println("clean source started");
-        prgBar.progressProperty().unbind();
-        prgBar.setProgress(0);
-
-        worker.cancel(true);
-
-        ready.setValue(Boolean.FALSE);
-        btnStart.setDisable(true);
-        btnStop.setDisable(true);
-        btnResume.setDisable(true);
-        btnPause.setDisable(true);
-    }
-
-    private synchronized boolean isSuspended() {
-        return dwnSuspend;
-    }
-
-    private synchronized void setSuspended(boolean wait) {
-        dwnSuspend = wait;
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("download controller started");
     }
     
-    private synchronized boolean isStopped(){
-        return dwnStop;
+    /*====================FXML functions==========================================*/
+    @FXML
+    private void dwnStart(){
+        System.out.println("start not ready yet");
     }
     
-    private synchronized void setStopped(boolean stop){
-        dwnStop = stop;
+    @FXML
+    private void dwnPause(){
+        System.out.println("start not ready yet");
     }
     
-    private synchronized void resumeThread(){
-        System.out.println(thread.getName()+" return is called");
-        this.notify();
+    @FXML
+    private void dwnResume(){
+        System.out.println("start not ready yet");
     }
+    
+    @FXML
+    private void dwnCancel(){
+        System.out.println("start not ready yet");
+    }
+    
+    @FXML
+    private void openFile(){
+        System.out.println("start not ready yet");
+    }
+    /*=================End of FXML functions======================================*/
 }
--- src/xdmexamples/controller/FxmlDownloadController.java
+++ src/xdmexamples/controller/FxmlDownloadController.java
@@ -1,224 +1,121 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package xdmexamples.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.net.URL;
-import java.util.Optional;
 import java.util.ResourceBundle;
-import javafx.application.Platform;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.value.ObservableValue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.concurrent.Task;
+import static javafx.concurrent.Worker.State.RUNNING;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TitledPane;
-import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.Stage;
 import xdmexamples.preloader.Keys;
 
-/**
- * FXML Controller class
- *
- * @author sakit
- */
 public class FxmlDownloadController implements Initializable {
 
-    /*-------------------------------------FXML decleration---------------------------*/
+    /*================================FXML Decleration============================*/
     @FXML
     private TextField urlText;
     @FXML
     private Button btnDownload;
     @FXML
-    private Button btnCancel;
+    private Button btCancell;
     @FXML
     private ProgressIndicator prgIndicator;
     @FXML
-    private VBox vbox;
-    @FXML
     private ResourceBundle bundle;
-    /*----------------------------------End of FXML decleration-----------------------*/
+    @FXML
+    private VBox vbox;
+    /*============================End of FXML Decleration=========================*/
 
-    private Task worker;
-    private final BooleanProperty ready = new SimpleBooleanProperty(false);
-    private String fileName = "", fileLocation = "";
-    private TitledPane tp = null;
+    private String fileName;
+    private String fileLocation;
+    private final static ThreadGroup threads = new ThreadGroup("Downloads");
+    private Thread thread;
+    private Task<TitledPane> task;
 
-    /**
-     * Initializes the controller class.
-     *
-     * @param url
-     * @param rb
-     */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        bundle = rb;
-        urlText.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+    public void initialize(URL location, ResourceBundle resources) {
+        bundle = resources;
+
+        //add text change listener to control download button disable
+        urlText.textProperty().addListener((obser, oldValue, newValue) -> {
             btnDownload.setDisable(newValue.isEmpty());
         });
     }
 
+    /*============================FXML functions==================================*/
     @FXML
     private void download() {
-        ready.setValue(Boolean.FALSE);
-        while (fileName.isEmpty()) {
-            fileName = getFileName();
-            if (fileName == null) {
-                break;
-            }
-        }
-        while (fileLocation.isEmpty()) {
-            fileLocation = getFileLocation();
-            if (fileLocation == null) {
-                break;
-            }
-        }
-        if (fileName != null && fileLocation != null) {
-            prgIndicator.setVisible(true);
-            prgIndicator.setProgress(-1);
-            worker = createTask();
-            ready.addListener((ObservableValue<? extends Boolean> ov, Boolean t, Boolean t1) -> {
-                if (Boolean.TRUE.equals(t1)) {
-                    Platform.runLater(() -> {
-                        System.out.println("Download started");
-                        prgIndicator.setVisible(false);
-                        vbox.getChildren().add(tp);
-                        //ready.setValue(Boolean.FALSE);
-                    });
-                }
-            });
-        }
-        new Thread(worker).start();
+        String name = urlText.getText();
+        fileName = name.substring(name.lastIndexOf("/") + 1);
+        fileLocation = getFileLocation();
+
+        task = createWork(fileName, fileLocation);
+        prgIndicator.visibleProperty().bind(task.stateProperty().isEqualTo(RUNNING));
+
+        thread = new Thread(threads, task, fileName);
+        thread.setDaemon(true);
+        thread.start();
     }
 
     @FXML
     private void cancelAll() {
     }
-
-    private void showErrorMessage(String message, Exception... excp) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(bundle.getString(Keys.ERROR));
-        alert.setHeaderText(bundle.getString(Keys.PRG_NAME));
-        alert.setContentText(message);
-
-        if (excp.length > 0) {
-            // Create expandable Exception.
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            excp[0].printStackTrace(pw);
-            String exceptionText = sw.toString();
-
-            Label label = new Label(bundle.getString(Keys.EXCEPTİON_LABEL));
-
-            TextArea textArea = new TextArea(exceptionText);
-            textArea.setEditable(false);
-            textArea.setWrapText(true);
-
-            textArea.setMaxWidth(Double.MAX_VALUE);
-            textArea.setMaxHeight(Double.MAX_VALUE);
-            GridPane.setVgrow(textArea, Priority.ALWAYS);
-            GridPane.setHgrow(textArea, Priority.ALWAYS);
-
-            GridPane expContent = new GridPane();
-            expContent.setMaxWidth(Double.MAX_VALUE);
-            expContent.add(label, 0, 0);
-            expContent.add(textArea, 0, 1);
-
-            // Set expandable Exception into the dialog pane.
-            alert.getDialogPane().setExpandableContent(expContent);
-        }
-        alert.showAndWait();
-    }
-
-    private void showDialog(Alert.AlertType type, String title, String message) {
-        Alert alert = new Alert(type);
-        alert.setTitle(title);
-        alert.setHeaderText(bundle.getString(Keys.PRG_NAME));
-        alert.setContentText(message);
-        Stage st = (Stage) alert.getDialogPane().getScene().getWindow();
-        st.getIcons().add(new Image(getClass().getResourceAsStream("/resources/images/azer_emblem.png")));
-
-        if (type == Alert.AlertType.CONFIRMATION) {
-            ButtonType btnYes = new ButtonType(bundle.getString(Keys.YES), ButtonBar.ButtonData.YES);
-            ButtonType btnCnl = new ButtonType(bundle.getString(Keys.CANCEL), ButtonBar.ButtonData.CANCEL_CLOSE);
-
-            alert.getButtonTypes().setAll(btnYes, btnCnl);
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.get() == btnYes) {
-
-            }
-        } else {
-            alert.showAndWait();
-        }
-    }
-
-    private String getFileName() {
-        String fileName = null;
-        TextInputDialog dialog = new TextInputDialog();
-        dialog.setTitle(bundle.getString(Keys.FLNM));
-        dialog.setHeaderText(bundle.getString(Keys.PRG_NAME));
-        dialog.setContentText(bundle.getString(Keys.ENT_FLNM));
-        dialog.getEditor().setPromptText("example.txt");
-        // Traditional way to get the response value.
-        Optional<String> result = dialog.showAndWait();
-        if (result.isPresent()) {
-            fileName = result.get();
-        }
-        return fileName;
-    }
+    /*=======================End of FXML functions================================*/
 
     private String getFileLocation() {
         DirectoryChooser dc = new DirectoryChooser();
         dc.setInitialDirectory(new File(System.getProperty("user.home")));
         dc.setTitle(bundle.getString(Keys.DWN_MSG2));
         File location = dc.showDialog(btnDownload.getScene().getWindow());
-        return location != null ? location.getAbsolutePath() : null;
+        return location == null ? System.getProperty("user.home") : location.getAbsolutePath();
     }
 
-    private Task createTask() {
+    private Task createWork(String fileName, String fileLocation) {
         return new Task() {
+
+            private TitledPane ttlPane = null;
+
             @Override
-            protected Object call() throws Exception {
-                if (!isCancelled()) {
-
+            protected Object call() {
+                if (!task.isCancelled()) {
                     try {
+                        System.out.println("call started");
                         FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/fxmlControlDownload.fxml"), bundle);
-                        tp = (TitledPane) loader.load();
-                        FxmlControlDownloadController fcdc = loader.<FxmlControlDownloadController>getController();
-                        fcdc.initData(urlText.getText(), fileName, fileLocation);
-                        ready.setValue(Boolean.TRUE);
-                        //vbox.getChildren().add(tp);
-                        //ready.setValue(Boolean.FALSE);
-                    } catch (Exception ex) {
-                        showErrorMessage(ex.getMessage(), ex);
+                        ttlPane = (TitledPane) loader.load();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
                     }
-
-                } else {
-                    ready.setValue(Boolean.FALSE);
                 }
                 return true;
+            }
+
+            @Override
+            protected void cancelled() {
+                super.cancelled();
+                System.out.println(thread.getName() + " the task was cancelled");
+            }
+
+            @Override
+            protected void failed() {
+                super.failed();
+                System.out.println(thread.getName() + " the task was faild");
+            }
+
+            @Override
+            protected void succeeded() {
+                super.succeeded();
+                vbox.getChildren().add(ttlPane);
+                System.out.println(thread.getName() + " the task was succeeded");
             }
         };
     }
